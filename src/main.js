15fece6f19d4854f5f66e48b6d59371505743837
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-04-20 18:25:36 -0300

    criando a seção de planos

---
+++ src/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (botao) {
+      const targetTab = botao.target.dataset.tabButton;
+      const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
+      hideTabs();
+      tab.classList.add("shows__list--is-active");
+      removeButtonActive();
+      botao.target.classList.add("shows__tabs__button--is-active");
+    });
+  }
+});
+
+function hideTabs() {
+  const tabsContainer = document.querySelectorAll("[data-tab-id]");
+
+  for (let i = 0; i < tabsContainer.length; i++) {
+    tabsContainer[i].classList.remove("shows__list--is-active");
+  }
+}
+
+function removeButtonActive() {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("shows__tabs__button--is-active");
+  }
+}
