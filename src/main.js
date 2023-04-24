9fa771c1033eb19d500db905b0f1c7bf224a9796
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-04-24 18:02:44 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
+
+  const heroSection = document.querySelector(".hero");
+  const heroHeight = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const currentPosition = window.scrollY;
+
+    if (currentPosition < heroHeight) {
+      hideElements();
+    } else {
+      showElements();
+    }
+  });
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
@@ -11,7 +25,21 @@ document.addEventListener("DOMContentLoaded", function () {
       botao.target.classList.add("shows__tabs__button--is-active");
     });
   }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", toggleAnswer);
+  }
 });
+
+function hideElements() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function showElements() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
 
 function hideTabs() {
   const tabsContainer = document.querySelectorAll("[data-tab-id]");
@@ -26,4 +54,11 @@ function removeButtonActive() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].classList.remove("shows__tabs__button--is-active");
   }
+}
+
+function toggleAnswer(e) {
+  const classOpen = "faq__questions__item--is-open";
+  const targetParent = e.target.parentNode;
+
+  targetParent.classList.toggle(classOpen);
 }
